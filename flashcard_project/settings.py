8f105f8dd11d31947f3883ef97c18efb9593ddee
Dunstan M'mbehero <dunmmbehero@gmail.com>
2021-06-03 10:28:54 +0300

    configure postgres for deployment

--- flashcard_project/settings.py
+++ flashcard_project/settings.py
@@ -78,12 +78,26 @@ WSGI_APPLICATION = 'flashcard_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+
+if config('MODE')=="dev":
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Password validation
